b452b20e6db54411d59f797ce00ef653383cba30
Rafael <dalacquar@gmail.com>
2023-11-20 17:16:32 -0300

    Initial Commit

---
+++ src/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+function NewPage() {
+  return (
+    <Box p={4}>
+      <Text fontSize="xl">Esta é a nova página!</Text>
+      {/* Adicione mais componentes do Chakra UI aqui conforme necessário */}
+    </Box>
+  );
+}
+
+export default NewPage;
