20432d4a324d05ec102c8b8fb6ddfd8c14551f1c
Rafael <dalacquar@gmail.com>
2023-11-21 18:28:57 -0300

    Atualizações iniciais, criando o grid

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,13 +1,49 @@
-import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import React from "react";
+import {
+  Box,
+  Button,
+  Grid,
+  GridItem,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Portal,
+  Text,
+} from "@chakra-ui/react";
 
-function NewPage() {
+function Home() {
   return (
-    <Box p={4}>
-      <Text fontSize="xl">Esta é a nova página!</Text>
-      {/* Adicione mais componentes do Chakra UI aqui conforme necessário */}
-    </Box>
+    <Grid
+      templateAreas={`"header"
+                        "main"
+                        "footer"`}
+      gridTemplateRows={"15vh 70vh 15vh"}
+      backgroundColor={"#1B1928"}
+      height={"100vh"}
+    >
+      <GridItem bg="orange.300" area={"header"}>
+        <Menu>
+          <MenuButton>Open menu</MenuButton>
+          <Portal>
+            <MenuList>
+              <MenuItem>Times</MenuItem>
+              <MenuItem>Times</MenuItem>
+              <MenuItem>Open Closed Tab</MenuItem>
+              <MenuItem>Open File</MenuItem>
+            </MenuList>
+          </Portal>
+        </Menu>
+        Header
+      </GridItem>
+      <GridItem bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+      <GridItem bg="blue.300" area={"footer"}>
+        Footer
+      </GridItem>
+    </Grid>
   );
 }
 
-export default NewPage;
+export default Home;
